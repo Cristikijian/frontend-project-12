c6d29f10b4f6fb3589eb5b360ff52bbb9333b1b2
Cristikijian <cristikijian@gmail.com>
2023-06-21 14:02:00 +0300

    eslitProblemFix2

--- frontend/src/utils/normalizer.jsx
+++ frontend/src/utils/normalizer.jsx
@@ -1,12 +1,11 @@
 import { normalize, schema } from 'normalizr';
 
 const Normalizer = (data) => {
-    const message = new schema.Entity('messages');
-    const channel = new schema.Entity('channels', {
-      messages: [message],
-    });
-    return normalize(data, [channel]);
-  };
-
+  const message = new schema.Entity('messages');
+  const channel = new schema.Entity('channels', {
+    messages: [message],
+  });
+  return normalize(data, [channel]);
+};
 
 export default Normalizer;
